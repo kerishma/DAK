3069a68e7427a836cf413430facf5b69c47b384e
kerishma tarin <kerishma.tarin@gmail.com>
2020-10-22 21:46:54 -0700

    codeing

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-// The app will not render correctly until you setup a Route component.
-// Refer to the Basic Example documentation if you need to.
-// (https://reacttraining.com/react-router/web/example/basic)
 function App() {
   return (
-    <BrowserRouter>
-    <div>
-      <Nav />
-      <Switch>
-          <Route exact path="/books/:id" component={Detail} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/" component={Books} />
-          <Route path="*" component={NoMatch} />
-      </Switch>
-      <Books />
-    </div>
-    </BrowserRouter>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path={["/", "/books"]}>
+            <Books />
+          </Route>
+          <Route exact path="/books/:id">
+            <Detail />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
